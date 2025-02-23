cdc782609074d8bacc1b9799418266dfa24749f1
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-02-23 02:06:58 +0000

    added zsh

--- install.sh
+++ install.sh
@@ -19,3 +19,10 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+#######
+# ZSH #
+#######
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
